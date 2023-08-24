63d86b278c0a98e68336887452c47cb11c3eaedf
Kazuki Tobita <kazukiti201@gmail.com>
2023-08-24 22:25:48 +0900

    Delete unnecessary codes

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,6 @@
 
 require('dotenv').config()
 require('@nomiclabs/hardhat-ethers')
-// require('@nomiclabs/hardhat-etherscan')
 
 module.exports = {
   solidity: "0.8.9",
@@ -14,7 +13,4 @@ module.exports = {
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
-  // etherscan: {
-  //   apiKey: process.env.ETHERSCAN_API_KEY,
-  // },
 };
